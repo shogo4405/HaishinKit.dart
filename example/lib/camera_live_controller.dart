c20040279afd6441d97bed7bd7216e904932d325
SimonWang9610 <dengpan9610.wang@gmail.com>
2023-10-10 21:35:11 -0400

    split registerTexture logic & new example

---
+++ example/lib/camera_live_controller.dart
@@ -0,0 +1,425 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:haishin_kit/audio_settings.dart';
+import 'package:haishin_kit/audio_source.dart';
+import 'package:haishin_kit/av_capture_session_preset.dart';
+import 'package:haishin_kit/rtmp_connection.dart';
+import 'package:haishin_kit/rtmp_stream.dart';
+import 'package:haishin_kit/video_settings.dart';
+import 'package:haishin_kit/video_source.dart';
+
+import 'package:audio_session/audio_session.dart';
+
+import 'models/live_stream_state.dart';
+import 'models/rtmp_event.dart';
+
+class CameraLiveStreamController extends LiveStreamConnectionManager
+    with LiveStreamControls, LiveStreamTextureMixin {
+  CameraLiveStreamController(super.server, super.streamKey) {
+    _initialNotifyState();
+  }
+
+  @override
+  Future<void> dispose() async {
+    print("dispose camera live stream controller");
+    await _release();
+    super.dispose();
+  }
+
+  Future<void> _release() async {
+    switch (status) {
+      case LiveStreamStatus.living:
+      case LiveStreamStatus.connected:
+      case LiveStreamStatus.initialized:
+        await _releaseStreaming();
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+// todo: (feat) background usage, like pause/resume streaming
+class LiveStreamConnectionManager with LiveStreamNotifier {
+  final String server;
+  final String streamKey;
+
+  LiveStreamConnectionManager(this.server, this.streamKey);
+
+  RtmpConnection? _connection;
+  RtmpStream? _stream;
+
+  StreamSubscription? _connectionEventSub;
+  StreamSubscription? _streamEventSub;
+
+  bool _initialized = false;
+
+  bool get initialized => _initialized;
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    /// no need to dispose [RtmpStream] since it will be disposed when [RtmpConnection] is disposed
+    /// for android, all [RtmpStream] would be disposed when [RtmpConnection] is disposed
+    /// for iOS, [RtmpStream] would deinit when [RtmpConnection] is disposed (deinit)
+    ///
+
+    _connection?.dispose();
+    _connectionEventSub?.cancel();
+    _streamEventSub?.cancel();
+  }
+
+  Future<void> _initialize() async {
+    if (_initialized) return;
+
+    try {
+      await _createConnection();
+      await _createStream();
+
+      _initialized = true;
+      _notifyStateChange(status: LiveStreamStatus.initialized);
+    } catch (e, s) {
+      _reportException(e, s);
+      _initialized = false;
+    }
+  }
+
+  Future<void> _createConnection() async {
+    _connection ??= await RtmpConnection.create();
+
+    _connectionEventSub?.cancel();
+    _connectionEventSub = _connection!.eventChannel
+        .receiveBroadcastStream()
+        .listen(_onLiveStreamEvent);
+  }
+
+  Future<void> _createStream() async {
+    if (_connection == null) return;
+    _stream ??= await RtmpStream.create(_connection!);
+
+    _stream!.videoSettings = _state.nativeVideoSettings;
+
+    await _attachAudioAndVideo();
+
+    _streamEventSub?.cancel();
+    _streamEventSub = _stream!.eventChannel
+        .receiveBroadcastStream()
+        .listen(_onLiveStreamEvent);
+  }
+
+  Future<void> _publishTo(String streamKey) async {
+    if (_state.status == LiveStreamStatus.connected ||
+        _state.status == LiveStreamStatus.stopped) {
+      await _stream?.publish(streamKey);
+    }
+  }
+
+  ///! for iOS, all status events are notified by [RtmpConnection.eventChannel]
+  void _onLiveStreamEvent(dynamic data) async {
+    debugPrint("on live stream event: $data");
+    final map = <String, dynamic>{};
+
+    for (final entry in (data["data"] as Map).entries) {
+      final key = entry.key as String?;
+      if (key != null) {
+        map[key] = entry.value;
+      }
+    }
+
+    final event = RtmpEvent.fromMap(map);
+
+    if (event is RtmpConnectionEvent) {
+      _onConnectionEvent(event);
+    } else if (event is RtmpStreamEvent) {
+      _onStreamEvent(event);
+    } else {
+      _broadcastEvent(event);
+    }
+  }
+
+  void _onConnectionEvent(RtmpConnectionEvent event) async {
+    final status = event.code == RtmpConnectionCode.connectSuccess
+        ? LiveStreamStatus.connected
+        : LiveStreamStatus.disconnected;
+    _notifyStateChange(status: status);
+    _broadcastEvent(event);
+  }
+
+  void _onStreamEvent(RtmpStreamEvent event) {
+    final status = event.code == RtmpStreamCode.publishStart
+        ? LiveStreamStatus.living
+        : LiveStreamStatus.stopped;
+
+    _notifyStateChange(status: status);
+    _broadcastEvent(event);
+  }
+
+  /// this value may not be changed synchronously with [_attachAudioAndVideo] and [_detachAudioAndVideo]\
+  /// since the two methods are async between dart and native sides.
+  bool _resourceAttached = false;
+
+  Future<void> _attachAudioAndVideo() async {
+    if (_stream == null || _resourceAttached) return;
+
+    await Future.wait([
+      _stream!.attachAudio(AudioSource()),
+      _stream!.attachVideo(VideoSource(position: _state.cameraPosition)),
+    ]);
+
+    _resourceAttached = true;
+  }
+
+  Future<void> _detachAudioAndVideo() async {
+    if (_stream == null || !_resourceAttached) return;
+
+    await Future.wait([
+      _stream!.attachAudio(null),
+      _stream!.attachVideo(null),
+    ]);
+    print("detach audio and video");
+    _resourceAttached = false;
+  }
+}
+
+mixin LiveStreamNotifier {
+  final StreamController<LiveStreamState> _stateController =
+      StreamController<LiveStreamState>.broadcast();
+  final StreamController<RtmpEvent> _eventController =
+      StreamController<RtmpEvent>.broadcast();
+
+  Stream<LiveStreamState> get stateStream => _stateController.stream;
+  Stream<RtmpEvent> get eventStream => _eventController.stream;
+
+  LiveStreamState _state = const LiveStreamState();
+  LiveStreamState get state => _state;
+  LiveStreamStatus get status => _state.status;
+
+  void dispose() {
+    _stateController.close();
+    _eventController.close();
+  }
+
+  void _notifyStateChange({
+    LiveStreamStatus? status,
+    CameraPosition? cameraPosition,
+    Fps? frameRate,
+    AVCaptureSessionPreset? sessionPreset,
+    int? audioBitrate,
+    Bitrate? videoBitrate,
+    ProfileLevel? profileLevel,
+    int? frameInterval,
+    Resolution? videoResolution,
+  }) {
+    _state = _state.copyWith(
+      status: status,
+      cameraPosition: cameraPosition,
+      frameRate: frameRate,
+      sessionPreset: sessionPreset,
+      audioBitrate: audioBitrate,
+      videoBitrate: videoBitrate,
+      profileLevel: profileLevel,
+      frameInterval: frameInterval,
+      videoResolution: videoResolution,
+    );
+
+    debugPrint("state changed: $_state");
+
+    if (!_stateController.isClosed) {
+      _stateController.add(_state);
+    }
+  }
+
+  void _broadcastEvent(RtmpEvent event) {
+    debugPrint("RTMP event: $event");
+
+    if (!_eventController.isClosed) {
+      _eventController.add(event);
+    }
+  }
+
+  void _reportException(Object e, [StackTrace? s]) {
+    _stateController.addError(e, s);
+  }
+
+  void _initialNotifyState() {
+    _stateController.onListen = () {
+      _stateController.add(_state);
+    };
+  }
+}
+
+mixin LiveStreamControls on LiveStreamConnectionManager {
+  Future<void> initialize() async {
+    await _initialize();
+    _connection?.connect(server);
+  }
+
+  Future<void> startStreaming() async {
+    if (status == LiveStreamStatus.connected ||
+        status == LiveStreamStatus.stopped) {
+      await _attachAudioAndVideo();
+      await _publishTo(streamKey);
+    }
+  }
+
+  Future<void> stopStreaming() async {
+    if (status == LiveStreamStatus.living) {
+      await _releaseStreaming();
+    }
+  }
+
+  ///! on Android, should [_detachAudioAndVideo] before closing [RtmpStream],
+  ///! otherwise, the app will crash
+  Future<void> _releaseStreaming() async {
+    await _detachAudioAndVideo();
+    _notifyStateChange(status: LiveStreamStatus.stopped);
+  }
+
+  Future<void> switchCamera() async {
+    final position = _state.cameraPosition == CameraPosition.back
+        ? CameraPosition.front
+        : CameraPosition.back;
+
+    _notifyStateChange(cameraPosition: position);
+    _stream?.attachVideo(VideoSource(position: position));
+  }
+
+  Future<void> toggleAudio() async {
+    final hasAudio = await _stream?.getHasAudio();
+
+    if (hasAudio == null) return;
+    _stream?.setHasAudio(!hasAudio);
+  }
+
+  Future<void> setVideo({
+    Resolution? resolution,
+    ProfileLevel? profileLevel,
+    Bitrate? bitrate,
+  }) async {
+    final videoSetting = _state.nativeVideoSettings.copyWith(
+      width: resolution?.width,
+      height: resolution?.height,
+      profileLevel: profileLevel,
+      bitrate: bitrate != null ? bitrate.value * 1000 : null,
+    );
+
+    if (videoSetting != _state.nativeVideoSettings) {
+      _stream?.videoSettings = videoSetting;
+      _notifyStateChange(
+        videoResolution: resolution,
+        profileLevel: profileLevel,
+        videoBitrate: bitrate,
+      );
+    }
+  }
+
+  set audioBitrate(int value) {
+    if (_state.audioBitrate == value) {
+      return;
+    }
+    _stream?.audioSettings = AudioSettings(bitrate: value);
+    _notifyStateChange(audioBitrate: value);
+  }
+
+  /// iOS only, android will ignore this
+  set sessionPreset(AVCaptureSessionPreset preset) {
+    if (_state.sessionPreset == preset) {
+      return;
+    }
+    _stream?.sessionPreset = preset;
+    _notifyStateChange(sessionPreset: preset);
+  }
+
+  //! after changing frameRate, the rtmp connection will be closed
+  // android cannot set frameRate when living
+  set frameRate(Fps value) {
+    if (_state.frameRate == value || _state.status == LiveStreamStatus.living) {
+      return;
+    }
+
+    _notifyStateChange(frameRate: value);
+    _stream?.frameRate = value.value;
+  }
+}
+
+mixin LiveStreamTextureMixin on LiveStreamConnectionManager {
+  int? _textureId;
+  Size? _previousSize;
+
+  int? get textureId => _textureId;
+
+  bool _textureRegistered = false;
+  bool get textureRegistered => _textureRegistered;
+
+  Future<int?> registerTexture() async {
+    if (!initialized || _textureRegistered) {
+      return _textureId;
+    }
+
+    try {
+      _textureId ??= await _stream!.registerTexture({});
+
+      if (_textureId != null) {
+        _textureRegistered = true;
+      }
+    } catch (e) {
+      debugPrint("register texture failed: $e");
+      _textureRegistered = false;
+    }
+
+    return _textureId;
+  }
+
+  Future<void> unregisterTexture() async {
+    if (_textureId != null) {
+      await _stream?.unregisterTexture({
+        "id": _textureId,
+      });
+      _textureId = null;
+    }
+  }
+
+  Future<bool> updateTextureSize(Size? size) async {
+    if (_textureId == null) return false;
+
+    if (size != null && _previousSize != size) {
+      final textureId = await _stream!.updateTextureSize({
+        "width": size.width,
+        "height": size.height,
+      });
+
+      debugPrint(
+          "size updated: $size, texture changed: ${textureId != _textureId}");
+
+      assert(textureId == _textureId || textureId == null);
+
+      if (textureId != null) {
+        _previousSize = size;
+      }
+    }
+    return _previousSize == size;
+  }
+
+  @override
+  Future<void> dispose() async {
+    await unregisterTexture();
+    super.dispose();
+  }
+}
+
+Future<bool> configureAudioSession() async {
+  final session = await AudioSession.instance;
+  const config = AudioSessionConfiguration(
+    avAudioSessionCategory: AVAudioSessionCategory.playAndRecord,
+    avAudioSessionCategoryOptions: AVAudioSessionCategoryOptions.allowBluetooth,
+  );
+  try {
+    await session.configure(config);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,10 +9,25 @@ import 'package:haishin_kit/rtmp_connection.dart';
 import 'package:haishin_kit/rtmp_stream.dart';
 import 'package:haishin_kit/video_settings.dart';
 import 'package:haishin_kit/video_source.dart';
+import 'package:haishin_kit_example/camera_live_controller.dart';
+import 'package:haishin_kit_example/models/live_stream_state.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  const String server = "rtmp://192.168.68.52/live";
+  const streamKey = "SySD6EQb6";
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: WidgetWrapper(
+          child: Example2(
+            server: server,
+            streamKey: streamKey,
+          ),
+        ),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -56,10 +71,11 @@ class _MyAppState extends State<MyApp> {
 
     RtmpConnection connection = await RtmpConnection.create();
     connection.eventChannel.receiveBroadcastStream().listen((event) {
+      print("event: $event");
       switch (event["data"]["code"]) {
         case 'NetConnection.Connect.Success':
           if (_mode == "publish") {
-            _stream?.publish("live");
+            _stream?.publish("SySD6EQb6");
           } else {
             _stream?.play("live");
           }
@@ -135,10 +151,247 @@ class _MyAppState extends State<MyApp> {
                 _recording = false;
               });
             } else {
-              _connection?.connect("rtmp://192.168.1.9/live");
+              _connection?.connect("rtmp://192.168.68.52/live");
             }
           },
         ),
+      ),
+    );
+  }
+}
+
+class WidgetWrapper extends StatefulWidget {
+  final Widget child;
+  const WidgetWrapper({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  State<WidgetWrapper> createState() => _WidgetWrapperState();
+}
+
+class _WidgetWrapperState extends State<WidgetWrapper> {
+  bool _audioSessionConfigured = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  void _init() async {
+    await Permission.camera.request();
+    await Permission.microphone.request();
+
+    _audioSessionConfigured = await configureAudioSession();
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _audioSessionConfigured
+        ? Center(
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => widget.child,
+                  ),
+                );
+              },
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: const Text("Use Camera"),
+            ),
+          )
+        : const Center(
+            child: Text("Configuring audio session..."),
+          );
+  }
+}
+
+class Example2 extends StatefulWidget {
+  final String server;
+  final String streamKey;
+
+  const Example2({
+    super.key,
+    required this.server,
+    required this.streamKey,
+  });
+
+  @override
+  State<Example2> createState() => Example2State();
+}
+
+class Example2State extends State<Example2> {
+  late final CameraLiveStreamController _controller;
+
+  late StreamSubscription<LiveStreamState> _subscription;
+
+  late LiveStreamState _currentState;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraLiveStreamController(
+      widget.server,
+      widget.streamKey,
+    )..initialize();
+
+    _subscription = _controller.stateStream.listen((event) {
+      if (_currentState != event) {
+        setState(() {
+          _currentState = event;
+        });
+      }
+    });
+
+    _currentState = _controller.state;
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Live Stream Example"),
+      ),
+      body: _buildBodyView(),
+      floatingActionButton: _buildControlButton(),
+    );
+  }
+
+  Widget _buildBodyView() {
+    if (_currentState.status == LiveStreamStatus.idle) {
+      return const Center(
+        child: Text("Configuring Camera..."),
+      );
+    } else if (_currentState.status == LiveStreamStatus.connected ||
+        _currentState.status == LiveStreamStatus.initialized ||
+        _currentState.status == LiveStreamStatus.living) {
+      return LiveStreamPreview(
+        textureManager: _controller,
+        state: _currentState,
+      );
+    } else {
+      return const Center(
+        child: Text("Live stream stopped or disconnected."),
+      );
+    }
+  }
+
+  Widget? _buildControlButton() {
+    if (_currentState.status == LiveStreamStatus.idle) {
+      return FloatingActionButton(
+        onPressed: _controller.initialize,
+        child: const Text("Initialize"),
+      );
+    } else if (_currentState.status == LiveStreamStatus.connected) {
+      return FloatingActionButton(
+        onPressed: _controller.startStreaming,
+        child: const Text("Publish"),
+      );
+    } else if (_currentState.status == LiveStreamStatus.living) {
+      return FloatingActionButton(
+        onPressed: () async {
+          await _controller.stopStreaming();
+          Navigator.of(context).pop();
+        },
+        child: const Text("Stop"),
+      );
+    }
+
+    return null;
+  }
+}
+
+class LiveStreamPreview extends StatefulWidget {
+  final LiveStreamTextureMixin textureManager;
+  final LiveStreamState state;
+  const LiveStreamPreview({
+    super.key,
+    required this.textureManager,
+    required this.state,
+  });
+
+  @override
+  State<LiveStreamPreview> createState() => _LiveStreamPreviewState();
+}
+
+class _LiveStreamPreviewState extends State<LiveStreamPreview> {
+  int? _textureId;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.textureManager.textureId == null) {
+      _registerTexture();
+    } else {
+      _textureId = widget.textureManager.textureId;
+    }
+  }
+
+  void _updateTextureSize() {
+    final mediaSize = MediaQuery.of(context).size;
+
+    widget.textureManager.updateTextureSize(mediaSize);
+  }
+
+  void _registerTexture() async {
+    final textureId = await widget.textureManager.registerTexture();
+    setState(() {
+      _textureId = textureId;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_textureId == null) {
+      return const Center(
+        child: Text("No available video preview.\nProbably this device "
+            "does not support video preview."),
+      );
+    }
+    _updateTextureSize();
+
+    final orientation = MediaQuery.of(context).orientation;
+
+    final resolution = widget.state.videoResolution;
+
+    final aspectRatio = orientation == Orientation.portrait
+        ? 1 / resolution.aspectRatio
+        : resolution.aspectRatio;
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.red, width: 2),
+      ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          const ColoredBox(color: Colors.black),
+          Align(
+            child: AspectRatio(
+              aspectRatio: aspectRatio,
+              child: Texture(
+                textureId: _textureId!,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
